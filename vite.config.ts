1eb896b3c50b251c512f945eb515b2d132898fa1
Anthony Larson <anthony.r.larson@gmail.com>
2026-05-25 11:49:35 -0500

    fix amplify build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 const config = defineConfig({
@@ -23,10 +24,13 @@ const config = defineConfig({
 			},
 		},
 		tanstackStart(),
+		nitro({
+			preset: "aws-amplify",
+			rollupConfig: { external: [/^@sentry\//] },
+		}),
 		devtools(),
 		tailwindcss(),
 		viteReact(),
 	],
 });
-
 export default config;
